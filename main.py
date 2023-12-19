51d31d9dcb4ce1ea9b5383857c3bfdd7d27810b9
jpurser2727 <jpurser2727@gmail.com>
2023-12-19 10:49:50 -0600

    FINISH NOW

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
-trading_client = TradingClient('PKY2FY5HR5X7S7BUSHXV', 'lzAK9gwJ7M1eaAKT1teXt71YbqHxIYkKcbvwMj7y', paper=True)
+trading_client = TradingClient('PKANEOH958F3GJ2GS6N0', 'Wvp4HAcXoW8TYjIqh9CsWyF1zfB6WYdvVu3XTcKP', paper=True)
 
 # Define the ISS location API endpoint
 def get_iss_longitude():
@@ -13,68 +13,93 @@ def get_iss_longitude():
     return float(data['iss_position']['longitude'])
 
 # Initialize a dictionary to keep track of stock holdings
-myDict = {
-    'LMT': 0,  
-    'HD': 0    
-}
+
 
 def main():
     while True:
         iss_longitude = get_iss_longitude()
-        
+        print (get_iss_longitude())
         
         if -60 <= iss_longitude <= -40:
             # Buy 5 shares of Lockheed Martin stock
             print("working")
-            trading_client.submit_order(
-                symbol='LMT',
-                qty=5,
-                side=OrderSide.BUY,
-                time_in_force='gtc'
-            )
-            myDict['LMT'] += 5
-        elif -40 < iss_longitude <= -20:
-            # Sell 2 shares of Lockheed Martin stock
-            print("working")
-            if myDict['LMT'] >= 2:
-                trading_client.submit_order(
-                    symbol='LMT',
-                    qty=2,
+            market_order_data = MarketOrderRequest(
+                    symbol="LMT",
+                    qty=5,
                     side=OrderSide.BUY,
-                    time_in_force='gtc'
-                )
-                myDict['LMT'] -= 2
+                    time_in_force=TimeInForce.DAY
+                    )
+            market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+        
+        elif -40 < iss_longitude <= -20:
+             # sell 2 shares of Lockheed Martin stock
+            print("working")
+            market_order_data = MarketOrderRequest(
+                    symbol="LMT",
+                    qty=2,
+                    side=OrderSide.SELL,
+                    time_in_force=TimeInForce.DAY
+                    )
+            market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+           
         elif -20 < iss_longitude <= 0:
             print("working")
-            # Buy 5 shares of Home Depot stock
-            trading_client.submit_order(
-                symbol='HD',
-                qty=5,
-                side=OrderSide.BUY,
-                time_in_force='gtc'
-            )
-            myDict['HD'] += 5
+            # Buy 5 shares of Lockheed Martin stock
+            print("working")
+            market_order_data = MarketOrderRequest(
+                    symbol="HD",
+                    qty=5,
+                    side=OrderSide.BUY,
+                    time_in_force=TimeInForce.DAY
+                    )
+            market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+    
         elif 0 < iss_longitude <= 20:
             print("working")
-            # Sell 2 shares of Home Depot stock
-            if myDict['HD'] >= 2:
-                trading_client.submit_order(
-                    symbol='HD',
+ # Buy 5 shares of Lockheed Martin stock
+            print("working")
+            market_order_data = MarketOrderRequest(
+                    symbol="LMT",
+                    qty=2,
+                    side=OrderSide.SELL,
+                    time_in_force=TimeInForce.DAY
+                    )
+            market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+    
+        elif 20 < iss_longitude <= 100:
+            print("working")
+            # Buy 5 shares of Lockheed Martin stock
+            market_order_data = MarketOrderRequest(
+                    symbol="LMT",
                     qty=2,
                     side=OrderSide.BUY,
-                    time_in_force='gtc'
-                )
-                myDict['HD'] -= 2
-        elif 20 < iss_longitude <= 100:
-            
+                    time_in_force=TimeInForce.DAY
+                    )
+            market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+        elif iss_longitude <=-60:
             print("working")
-            trading_client.submit_order(
-                symbol='LMT',
-                qty=2,
-                side=OrderSide.BUY,
-                time_in_force='gtc'
-            )
-            myDict['LMT'] += 2
+            # Buy 5 shares of Lockheed Martin stock
+            print("working")
+            market_order_data = MarketOrderRequest(
+                    symbol="LMT",
+                    qty=2,
+                    side=OrderSide.BUY,
+                    time_in_force=TimeInForce.DAY
+                    )
+            market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+            
 
 if __name__ == '__main__':
     main()
